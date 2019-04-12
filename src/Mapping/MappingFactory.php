505066576da015e97c23dd74e861930710adec63
Krzysztof Sowa <sowiq86@gmail.com>
2019-04-12 18:02:57 +0200

    Made URL not required

--- src/Mapping/MappingFactory.php
+++ src/Mapping/MappingFactory.php
@@ -142,9 +142,10 @@ class MappingFactory
     protected static function getSelfUrl(array &$mappedClass)
     {
         if (empty($mappedClass[static::URLS_KEY][static::SELF_KEY])) {
-            throw new MappingException(
-                'Could not find "self" property under "urls". This is required in order to make the resource to be reachable.'
-            );
+            return null;
+//            throw new MappingException(
+//                'Could not find "self" property under "urls". This is required in order to make the resource to be reachable.'
+//            );
         }
 
         return $mappedClass[static::URLS_KEY][static::SELF_KEY];
@@ -297,6 +298,10 @@ class MappingFactory
      */
     protected static function getOtherUrls(array $mappedClass)
     {
+        if (!isset($mappedClass[static::URLS_KEY]) || !isset($mappedClass[static::URLS_KEY][static::SELF_KEY])) {
+            return [];
+        }
+
         if (!empty($mappedClass[static::URLS_KEY][static::SELF_KEY])) {
             unset($mappedClass[static::URLS_KEY][static::SELF_KEY]);
         }
